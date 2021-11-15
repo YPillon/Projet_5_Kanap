a7b5b3c6a037ac90da12e3b3a767e6f1d7ab660f
YounesP <younespillon@gmail.com>
2021-11-15 10:08:58 +0100

    Début de l'implémentation des RegEx du formulaire.

--- front/js/cart.js
+++ front/js/cart.js
@@ -5,23 +5,14 @@ const panierRaw = localStorage.getItem("panier");
 const panierClean = JSON.parse(panierRaw);
 //console.log(panierClean);
 
+//
+//
 //Fonction cherchant les données produits liées à l'ID
 async function getOneProduct(articleId) {
   return fetch(`http://localhost:3000/api/products/${articleId}`)
     .then((res) => res.json())
     .catch((err) => console.log("Erreur: " + err));
 }
-
-//Je veux:
-//-Sélectionner les sélecteurs dans le HTML*
-//-Pour chaque sélecteur*
-//-Ecouter un changement de quantité*
-//-Récupérer la nouvelle valeur*
-//-Récupérer l'Id et la couleur du produit avec dataset()
-//-récupérer le panier
-//-chercher l'objet correspondant dans le panier avec find()
-//-affecter la nouvelle valeur
-//-renvoyer le panier
 
 //
 //
@@ -67,6 +58,12 @@ function updateValue(e) {
     localStorage.setItem("panier", JSON.stringify(panierToUpdate));
     console.log(localStorage.getItem("panier"));
   }
+
+  //On recalcule le prix total
+  calculatePrice();
+
+  //On recalcule la quantité totale
+  calculateItemsQuantity();
 }
 
 //
@@ -75,7 +72,7 @@ function updateValue(e) {
 function deleteItem(e) {
   //On sélectionne le noeud parent possédant "data-id" et "data-color"
   const produit = e.target.closest(".cart__item");
-  console.log(produit);
+  //console.log(produit);
 
   //On récupère l'id et la couleur
   const idToDelete = produit.dataset.id;
@@ -83,35 +80,41 @@ function deleteItem(e) {
 
   //On récupère le panier et on le parse
   const panierToUpdate = JSON.parse(localStorage.getItem("panier"));
-  console.log(panierToUpdate);
+  //console.log(panierToUpdate);
 
   //On recherche dans le panier l'index d' l'élément ayant le même ID et la même couleur que l'objet sélectionné
   const indexToDelete = panierToUpdate.findIndex(
     (article) => article.id === idToDelete && article.color === colorToDelete
   );
-  console.log('recherche index: ' + indexToDelete);
+  //console.log("recherche index: " + indexToDelete);
 
   //On supprime l'élément
   panierToUpdate.splice(indexToDelete, 1);
-  console.log(panierToUpdate);
+  //console.log(panierToUpdate);
 
   //On supprime l'ancien panier au niveau du LocalStorage
   localStorage.removeItem("panier");
 
   //On envoie le nouveau panier dans le localStorage
   localStorage.setItem("panier", JSON.stringify(panierToUpdate));
-  console.log(localStorage.getItem("panier"));
+  //console.log(localStorage.getItem("panier"));
 
   //On sélectionne le conteneur des produits dans le DOM
   const $articlesContainer = document.getElementById("cart__items");
 
   //On supprime le produit dans le DOM
   $articlesContainer.removeChild(produit);
+
+  //On recalcule le prix total
+  calculatePrice();
+
+  //On recalcule la quantité totale
+  calculateItemsQuantity();
 }
 
 //
 //
-//Je crée une fonction insérant les donnée dans la page panier
+//Je crée une fonction insérant les données dans la page panier
 function insertArticleInCart(article, articlePanier) {
   //Je sélectionne le conteneur des produits
   const $articlesContainer = document.getElementById("cart__items");
@@ -174,7 +177,6 @@ function insertArticleInCart(article, articlePanier) {
   $quantitySelector.setAttribute("min", "1");
   $quantitySelector.setAttribute("max", "100");
   $quantitySelector.setAttribute("value", `${articlePanier.quantity}`);
-
   //Ajout de la modification de la quantité
   $quantitySelector.addEventListener("change", updateValue);
 
@@ -187,7 +189,6 @@ function insertArticleInCart(article, articlePanier) {
   const $deleteButton = document.createElement("p");
   $deleteButton.classList.add("deleteItem");
   $deleteButton.textContent = "Supprimer";
-
   //Ajout de la suppression de l'article
   $deleteButton.addEventListener("click", deleteItem);
 
@@ -242,5 +243,70 @@ for (var articleInCart of panierClean) {
 }
 
 //
-//Partie gérant le changement de quantité et la suppression de produit dans le panier
 //
+//Fonction calculant le prix total à afficher
+async function calculatePrice() {
+  //On récupère le panier et on le parse
+  const panier = JSON.parse(localStorage.getItem("panier"));
+  //console.log(panier);
+
+  //On initialise la variable qui comptabilisera tous les prix
+  let allArticlesPrice = 0;
+
+  //On parcoure le panier
+  for (let item of panier) {
+    //On récupère les données de l'article via l'API
+    const articleData = await getOneProduct(item.id);
+    //console.log(articleData);
+
+    //On récupère le prix de l'article
+    var articlePrice = articleData.price;
+
+    //On multiplie le prix de l'article par sa quantité
+    var articleTotalPrice = articlePrice * item.quantity;
+    //console.log(item.quantity);
+    //console.log(articleTotalPrice);
+
+    //On additionne la valeur au prix total de tous les articles
+    allArticlesPrice += articleTotalPrice;
+    //console.log(allArticlesPrice);
+  }
+  //On sélectionne l'élément correspondant au prix total dans le DOM et on y affecte la valeur
+  const $totalPrice = document.getElementById("totalPrice");
+  $totalPrice.innerText = allArticlesPrice;
+}
+
+//
+//
+//Fonction calculant la quantité totale d'articles à afficher
+function calculateItemsQuantity() {
+  //On récupère le panier et on le parse
+  const panier = JSON.parse(localStorage.getItem("panier"));
+  //console.log(panier);
+
+  //On initialise la variable qui comptabilisera le nombre total d'articles
+  let allArticlesQuantity = 0;
+
+  for (let item of panier) {
+    allArticlesQuantity += parseInt(item.quantity, 10);
+    //console.log(allArticlesQuantity);
+  }
+  //On sélectionne l'élément correspondant au nombre total d'articles dans le DOM
+  const $totalNumberOfArticles = document.getElementById("totalQuantity");
+  $totalNumberOfArticles.innerText = allArticlesQuantity;
+}
+
+calculatePrice();
+calculateItemsQuantity();
+
+
+//--------------------------------Gestion du formulaire--------------------------------//
+
+//Fonction vérifiant si l'email est valide
+function isEmailValid(email) {
+  return /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email)
+}
+
+let mail = 'aiohao@sldln.com';
+console.log(isEmailValid('azbkbaf'));
+
